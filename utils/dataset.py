8592f096f13971f034a051de8018172233a1921f
Matt <mja78@cam.ac.uk>
2021-05-08 17:10:33 +0100

    Working point-cloud based dataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -123,3 +123,132 @@ class TreeSpeciesDataset(Dataset):
         sample = {'depth_images': depth_images, 'labels': labels}
 
         return sample
+    
+    
+class TreeSpeciesPointDataset(Dataset):
+    """Dataset for tree species classification from
+    point cloud depth projection images"""
+
+    def __init__(self, data_dir, metadata_file):
+        """
+        Args:
+            metadata_file (string): Path to the metadata file.
+            root_dir (string): Directory with all the images.
+        """
+        self.meta_frame = pd.read_csv(metadata_file, keep_default_na=False)
+        self.species = list(self.meta_frame.groupby('sp')['id'].nunique().keys())
+        
+        self.counts = self.meta_frame['sp'].value_counts() 
+        
+        self.data_dir = data_dir
+        
+        self.point_clouds = []
+        self.labels = None
+        
+        self.image_dim = None
+        self.camera_fov_deg = None
+        self.f = None
+        self.camera_dist = None
+        self.soft_min_k = None
+        
+        filenames = list(filter(lambda t:t.endswith('.txt'), os.listdir(self.data_dir)))
+        no_files = len(filenames)
+        
+        self.labels = torch.zeros(no_files)
+        
+        for i, file in tqdm(enumerate(filenames), total=no_files):
+            cloud = utils.pc_from_txt(self.data_dir + file)
+            cloud = utils.center_and_scale(cloud)
+            
+            self.point_clouds.append(torch.from_numpy(cloud))
+            
+            meta_entry = self.meta_frame[self.meta_frame.id==file[:-4]]
+            self.labels[i] = self.species.index(meta_entry.sp.values[0])
+            
+        self.labels = self.labels.long()
+        
+        return
+    
+    
+    def get_depth_image(self, i):
+        return torch.unsqueeze(
+               utils.get_depth_images_from_cloud(points=self.point_clouds[i], 
+                                                 image_dim=self.image_dim, 
+                                                 camera_fov_deg=self.camera_fov_deg, 
+                                                 f=self.f, 
+                                                 camera_dist=self.camera_dist
+                                                 )
+                                    , 1)
+    
+    
+    def remove_species(self, species):
+        
+        idx = [] #Indices to keep
+        
+        for i in range(len(self.labels)): #Remove entries in images and labels for that species
+            if not(self.species[int(self.labels[i])] == species):
+                idx.append(i)
+                
+        self.point_clouds = [self.point_clouds[i] for i in idx]
+        self.labels = self.labels[idx]
+                
+        old_species = self.species.copy() 
+        self.species.pop(self.species.index(species)) #Pop from species list
+            
+        species_map = [self.species.index(species) if species in self.species else None for species in old_species]     
+            
+
+        for k in range(len(self.labels)): #Apply species map to relabel
+            self.labels[k] = torch.tensor(species_map[int(self.labels[k])])
+            
+        self.counts = self.counts.drop(species)
+        
+        return
+    
+    def set_params(self, 
+                   image_dim = None,
+                   camera_fov_deg = None,
+                   f = None,
+                   camera_dist = None,
+                   soft_min_k = None):
+     
+        if image_dim:    
+            self.image_dim = image_dim        
+        if camera_fov_deg:
+            self.camera_fov_deg = camera_fov_deg 
+        if f:
+            self.f = f      
+        if camera_dist:
+            self.camera_dist = camera_dist      
+        if soft_min_k:
+            self.soft_min_k = soft_min_k
+            
+        return
+            
+    def __len__(self):
+        if self.labels == None:
+            return len(self.meta_frame)
+        else:
+            return len(self.labels)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+            num_trees = len(idx)
+        elif type(idx) == int:
+            num_trees = 1
+        
+
+        depth_images = torch.zeros(size=(num_trees, 6, 1, self.image_dim, self.image_dim))
+        
+        if type(idx) == list:
+            for i in range(len(idx)):
+                depth_images[i] = self.get_depth_image(int(idx[i]))
+        elif type(idx) == int:
+            depth_images = self.get_depth_image(idx)
+        
+        labels = self.labels[idx]
+        
+        sample = {'depth_images': depth_images, 'labels': labels}
+
+        return sample    
